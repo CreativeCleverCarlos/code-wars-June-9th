144ebbb0efaad3e07e2c0530a89459438cfa4bf4
Carlos Campbell <carlos.g.campbell@gmail.com>
2023-06-09 11:47:22 -0400

    the full solution

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 The input string will only consist of lower case letters and/or spaces.
 **/
+
+ function getCount(str) {
+    return (str.match(/[aeiou]/ig)||[]).length; //the ||[] is the 'or' operator that returns a value of 0 if there is no a,e,i,o,u
+  }
+
+  console.log(getCount('Hello thur'))
